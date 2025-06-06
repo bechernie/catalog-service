299b20c95baa9f430b1041113cf359887b14b9cf
bechernie <bechernie@gmail.com>
2025-06-06 18:17:36 +0200

    initial commit

---
+++ src/test/java/com/github/bechernie/catalogservice/CatalogServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.github.bechernie.catalogservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CatalogServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
