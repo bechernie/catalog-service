54a0e793ddde1fa72b8bb3f5d7b7462679bb5ff4
bechernie <bechernie@gmail.com>
2025-06-07 13:22:53 +0200

    testcontainers for integration tests

---
+++ src/test/java/com/github/bechernie/catalogservice/BookRepositoryJdbcTests.java
@@ -0,0 +1,36 @@
+package com.github.bechernie.catalogservice;
+
+import com.github.bechernie.catalogservice.config.DataConfig;
+import com.github.bechernie.catalogservice.domain.Book;
+import com.github.bechernie.catalogservice.domain.BookRepository;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.data.jdbc.DataJdbcTest;
+import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
+import org.springframework.context.annotation.Import;
+import org.springframework.data.jdbc.core.JdbcAggregateTemplate;
+import org.springframework.test.context.ActiveProfiles;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+@DataJdbcTest
+@Import(DataConfig.class)
+@AutoConfigureTestDatabase(replace = AutoConfigureTestDatabase.Replace.NONE)
+@ActiveProfiles("integration")
+public class BookRepositoryJdbcTests {
+    @Autowired
+    private BookRepository bookRepository;
+
+    @Autowired
+    private JdbcAggregateTemplate jdbcAggregateTemplate;
+
+    @Test
+    void findBookByIsbnWhenExisting() {
+        final var bookIsbn = "1234567890";
+        final var book = Book.of(bookIsbn, "Title", "Author", 9.90);
+        jdbcAggregateTemplate.insert(book);
+        final var actualBook = bookRepository.findByIsbn(bookIsbn);
+        assertThat(actualBook).isPresent();
+        assertThat(actualBook.get().isbn()).isEqualTo(book.isbn());
+    }
+}
