49d50d1585bdfe811c7c42fc6751841346c746ad
bechernie <bechernie@gmail.com>
2025-06-13 13:14:48 +0200

    security

---
+++ src/main/java/com/github/bechernie/catalogservice/config/SecurityConfig.java
@@ -0,0 +1,40 @@
+package com.github.bechernie.catalogservice.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
+import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.oauth2.server.resource.authentication.JwtAuthenticationConverter;
+import org.springframework.security.oauth2.server.resource.authentication.JwtGrantedAuthoritiesConverter;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+@EnableWebSecurity
+public class SecurityConfig {
+
+    @Bean
+    SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+        return http
+                .authorizeHttpRequests(authorizationManagerRequestMatcherRegistry -> authorizationManagerRequestMatcherRegistry
+                        .requestMatchers(HttpMethod.GET, "/", "/books/**").permitAll().anyRequest().hasRole("employee")
+                        .anyRequest().authenticated())
+                .oauth2ResourceServer(oAuth2ResourceServer -> oAuth2ResourceServer.jwt(Customizer.withDefaults()))
+                .sessionManagement(sessionManagement -> sessionManagement.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
+                .csrf(AbstractHttpConfigurer::disable)
+                .build();
+    }
+
+    @Bean
+    JwtAuthenticationConverter jwtAuthenticationConverter() {
+        final var jwtGrantedAuthorityConverter = new JwtGrantedAuthoritiesConverter();
+        jwtGrantedAuthorityConverter.setAuthorityPrefix("ROLE_");
+        jwtGrantedAuthorityConverter.setAuthoritiesClaimName("roles");
+        final var jwtAuthenticationConverter = new JwtAuthenticationConverter();
+        jwtAuthenticationConverter.setJwtGrantedAuthoritiesConverter(jwtGrantedAuthorityConverter);
+        return jwtAuthenticationConverter;
+    }
+}
